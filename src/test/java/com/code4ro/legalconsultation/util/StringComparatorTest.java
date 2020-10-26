1db18ebe90332cb9e62616c19f0c1dcf050490a0
Hassan Elseoudy <hassan.elseoudy@gmail.com>
2020-10-26 04:20:03 +0200

    Parameterized Tests

--- src/test/java/com/code4ro/legalconsultation/util/StringComparatorTest.java
+++ src/test/java/com/code4ro/legalconsultation/util/StringComparatorTest.java
@@ -12,18 +12,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StringComparatorTest {
 
     @ParameterizedTest(name="#{index} - Test with Argument={0}")
-    @CsvSource({" test test1 test2     ,   test      test ", " test test1 test2     ,   test      1 ", "test123,test",
-            " test     ,   test ", "test,test"})
-    void shouldCheckContainsTwoStringsWithDifferentWhitespaces(String str1, String str2) {
+    @CsvSource({"' test test1 test2     ','   test      test '", "' test test1 test2     ','   test      1 '", "test123,test",
+            "' test     ','   test '", "test,test"})
+    void shouldCheckTrueComparisonsContainsIgnoreWhitespace(String str1, String str2) {
         assertThat(StringComparator.containsIgnoreWhitespace(str1, str2)).isTrue();
     }
 
 
     @ParameterizedTest(name="#{index} - Test with Argument={0}")
-    @CsvSource({"test,test1", " test1     ,   test ", "test1,test"})
-    void shouldCheckNotContainsTwoStringsWithoutWhitespaces(String str1, String str2) {
+    @CsvSource({"test,test1"})
+    void shouldCheckFalseComparisonsContainsIgnoreWhitespace(String str1, String str2) {
         assertThat(StringComparator.containsIgnoreWhitespace(str1, str2)).isFalse();
     }
+
+    @ParameterizedTest(name="#{index} - Test with Argument={0}")
+    @CsvSource({"test,test", "' test     ','   test "})
+    void shouldCheckTrueComparisonsAreEqualIgnoreCaseAndWhitespace(String str1, String str2) {
+        assertThat(StringComparator.areEqualIgnoreCaseAndWhitespace(str1, str2)).isTrue();
+    }
+
+
+    @ParameterizedTest(name="#{index} - Test with Argument={0}")
+    @CsvSource({"test1,test", "' test1     ','   test "})
+    void shouldCheckFalseComparisonsAreEqualIgnoreCaseAndWhitespace(String str1, String str2) {
+        assertThat(StringComparator.areEqualIgnoreCaseAndWhitespace(str1, str2)).isFalse();
+    }
+
 
 
 }
