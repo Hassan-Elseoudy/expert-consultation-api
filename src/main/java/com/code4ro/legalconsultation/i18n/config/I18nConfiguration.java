8597659a22431f81cd8448963edef5c8c905ff96
Hassan Elseoudy <hassan.elseoudy@gmail.com>
2020-10-26 20:56:17 +0200

    Ignoring Whitespaces

--- src/main/java/com/code4ro/legalconsultation/i18n/config/I18nConfiguration.java
+++ src/main/java/com/code4ro/legalconsultation/i18n/config/I18nConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class I18nConfiguration implements WebMvcConfigurer {
+public class I18nConfiguration implements WebMvcConfigurer{
 
     @Bean
     public ReloadableResourceBundleMessageSource messageSource() {
